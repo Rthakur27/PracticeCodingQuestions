1f7fd16c67b666ee495c54a16194ea2334a22634
Richa Thakur <richathakur914@gmail.com>
2023-10-14 11:29:45 +0530

    3 angles make triangle

---
+++ code9.py
@@ -0,0 +1,8 @@
+angle_1 = float(input("Enter first angle: "))
+angle_2 = float(input("Enter second angle: "))
+angle_3 = float(input("Enter third angle: "))
+
+if angle_1 + angle_2 + angle_3 == 180:
+    print("These angles can form a triangle.")
+else:
+    print("These angles cannot form a triangle.")
