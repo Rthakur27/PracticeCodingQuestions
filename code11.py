3cbe7bf44492460e52cf702627445ffa4dee4a93
Richa Thakur <richathakur914@gmail.com>
2023-10-15 10:54:38 +0530

    Simple Interest

---
+++ code11.py
@@ -0,0 +1,6 @@
+principal = float(input("Enter the principal amount: "))
+rate_of_interest = float(input("Enter the rate of interest (as a decimal): "))
+time_period = float(input("Enter the time period (in years): "))
+
+simple_interest = (principal * rate_of_interest * time_period)
+print(f"Simple Interest: {simple_interest}")
